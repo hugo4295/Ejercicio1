3846753db8f0b5020ad11ab76df1b98c05b30817
hugo4295 <hugo_4295@hotmail.com>
2020-04-03 13:27:46 -0600

    primer ejercicio

---
+++ app/src/main/java/mx/edu/isc/tesoem/hugo4295/ejercicio1/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.isc.tesoem.hugo4295.ejercicio1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
